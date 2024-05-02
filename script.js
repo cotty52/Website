57d411a2f8232ba6a0a56d0fd6c90c5c0f8a9054
cotty52 <cjo1102.52@gmail.com>
2024-05-02 15:28:51 -0400

    stuff?

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 const ShowPage = (pageName, btnName) => {
   //Future proofing in case more pages and buttons are added
-  var pageContent, buttonLink, i;
+  var pageContent, i;
   pageContent = document.getElementsByClassName("content");
   for (i = 0; i < pageContent.length; i++) {
     pageContent[i].style.display = "none";
   }
 
   // Get id of previously active button, then remove the active class
-  const activeButton = document.querySelector('nav button.active');
-  activeButton.classList.remove('active');
+  const activeButton = document.querySelector("nav button.active");
+  activeButton.classList.remove("active");
 
   // Get id of clicked button and add 'active' class
   const clickedButton = document.getElementById(btnName);
-  clickedButton.classList.add('active');
+  clickedButton.classList.add("active");
 
   // Get id of the new page and show it
   const newPage = document.getElementById(pageName);
   newPage.style.display = "block";
 
+  // Construct the new URL
+  const newUrl = window.location.origin + "/#" + pageName;
+
+  // Update the URL without triggering a page reload
+  window.history.pushState({ path: newUrl }, "", newUrl);
 };
 // Get the element with id="defaultOpen" and click on it
 //document.getElementById("homeBtn").click();

d457a4a0a4f7e836143daae09ad3325f5e4cd1b3
cotty52 <cjo1102.52@gmail.com>
2024-06-17 15:17:14 -0400

    Works I think?

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 SiteLoad = () => {
   // When site is loaded, press that button. Could also call ShowPage
   document.getElementById("designsBtn").click();
+  InitializeAllSliders();
 };
 
 const ShowPage = (pageName, btnName) => {
@@ -31,104 +32,103 @@ const ShowPage = (pageName, btnName) => {
   window.history.pushState({ path: newUrl }, "", newUrl);
 };
 
-// function scrollFunction() {
-//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-//     document.getElementById("navbar").style.padding = "30px 10px";
-//     document.getElementById("logo").style.fontSize = "25px";
-//   } else {
-//     document.getElementById("navbar").style.padding = "80px 10px";
-//     document.getElementById("logo").style.fontSize = "35px";
-//   }
-// }
-
 // IMAGE SLIDER
+class Slider {
+  constructor(sliderContainer) {
+    this.sliderContainer = sliderContainer;
+    this.slides = sliderContainer.querySelectorAll('.slides');
+    this.navButtons = sliderContainer.querySelectorAll('.sliderDot');
+    this.prevButton = sliderContainer.querySelector('.prev');
+    this.nextButton = sliderContainer.querySelector('.next');
+    this.slideIndex = 0;
+    this.prevShown = 0;
+    this.length = this.slides.length;
 
-const numSlides = document.querySelector(".slider-container").querySelectorAll(".slides");
-const length = numSlides.length;
-const navButtons = document.querySelector(".slider-nav").querySelectorAll(".sliderDot");
-let slideIndex = 0;
-let prevShown = 0;
+    this.initializeSlider();
 
-InitializeSlider();
-
-function InitializeSlider() {
-  if (length > 0) {
-    numSlides[slideIndex].classList.add("displaySlide");
-    navButtons[slideIndex].classList.add("current");
+    // Bind events
+    this.prevButton.addEventListener('click', () => this.prevSlide());
+    this.nextButton.addEventListener('click', () => this.nextSlide());
+    this.navButtons.forEach((button, index) => {
+      button.addEventListener('click', () => this.changeSlide(index));
+    });
   }
-  console.log("slideIndex: " + slideIndex); // Check the current slide
+
+  initializeSlider() {
+    if (this.length > 0) {
+      this.slides[this.slideIndex].classList.add('displaySlide');
+      this.navButtons[this.slideIndex].classList.add('current');
+    }
+    console.log('slideIndex: ' + this.slideIndex); // Check the current slide
+  }
+
+  showSlide(index, direction) {
+    this.sliderContainer.style.pointerEvents = 'none';
+
+    if (index >= this.length) {
+      this.slideIndex = 0;
+      this.prevShown = this.length - 1;
+    } else if (index < 0) {
+      this.slideIndex = this.length - 1;
+      this.prevShown = 0;
+    } else {
+      this.prevShown = this.slideIndex;
+      this.slideIndex = index;
+    }
+
+    const prevSlide = this.slides[this.prevShown];
+    const newSlide = this.slides[this.slideIndex];
+
+    if (direction === 'left') {
+      newSlide.classList.add('move-right-in', 'displaySlide', 'z-offset');
+      prevSlide.classList.add('move-right-out');
+    } else if (direction === 'right') {
+      newSlide.classList.add('move-left-in', 'displaySlide', 'z-offset');
+      prevSlide.classList.add('move-left-out');
+    }
+
+    this.navButtons.forEach(sliderDot => {
+      sliderDot.classList.remove('current');
+    });
+
+    setTimeout(() => {
+      this.sliderContainer.style.pointerEvents = 'auto';
+      prevSlide.classList.remove('displaySlide');
+      this.navButtons[this.slideIndex].classList.add('current');
+      this.slides.forEach(slide => {
+        slide.classList.remove('move-left-in', 'move-right-in', 'move-left-out', 'move-right-out');
+      });
+    }, 510);
+    setTimeout(() => {
+      this.slides.forEach(slide => {
+        slide.classList.remove('z-offset');
+      });
+    }, 510);
+  }
+
+  prevSlide() {
+    this.showSlide(this.slideIndex - 1, 'left');
+  }
+
+  nextSlide() {
+    this.showSlide(this.slideIndex + 1, 'right');
+  }
+
+  changeSlide(index) {
+    let direction = 'left';
+    if (index === this.slideIndex) {
+      return;
+    } else if (index > this.slideIndex) {
+      direction = 'right';
+    }
+    this.showSlide(index, direction);
+  }
 }
 
-function ShowSlide(index, prevShown, direction) {
-  const sliderContainer = document.querySelector(".slider-container");
-
-  // Disable pointer events before animation
-  sliderContainer.style.pointerEvents = "none";
-  
-  if (index >= length) {
-    slideIndex = 0;
-    prevShown = length - 1;
-  } else if (index < 0) {
-    slideIndex = length - 1;
-    prevShown = 0;
-  }
-
-  const prevSlide = numSlides[prevShown];
-  const newSlide = numSlides[slideIndex];
-
-  if (direction === "left") {
-    newSlide.classList.add("move-right-in", "displaySlide", "z-offset");
-    prevSlide.classList.add("move-right-out");
-  } else if (direction === "right") {
-    
-    newSlide.classList.add("move-left-in", "displaySlide", "z-offset");
-    prevSlide.classList.add("move-left-out");
-  }
-
-  navButtons.forEach(sliderDot => {
-    sliderDot.classList.remove("current");
+// Initialize all sliders on the page
+document.addEventListener('DOMContentLoaded', () => {
+  const sliders = document.querySelectorAll('.slider-container');
+  sliders.forEach(sliderContainer => {
+    new Slider(sliderContainer);
   });
-
-  setTimeout(() => {
-    sliderContainer.style.pointerEvents = "auto";
-    prevSlide.classList.remove("displaySlide");
-    navButtons[slideIndex].classList.add("current");
-    numSlides.forEach(slides => {
-      slides.classList.remove("move-left-in", "move-right-in", "move-left-out", "move-right-out");
-    });
-  }, 510);
-  setTimeout(() => {
-    numSlides.forEach(slides => {
-      slides.classList.remove("z-offset");
-    });
-  }, 510); 
-
-}
-
-function PrevSlide() {
-  prevShown = slideIndex;
-  slideIndex--;
-  ShowSlide(slideIndex, prevShown, "left");
-}
-
-function NextSlide() {
-  prevShown = slideIndex;
-  slideIndex++;
-  ShowSlide(slideIndex, prevShown, "right");
-}
-
-function ChangeSlide(index) {
-  let direction = "left";
-  if (index == slideIndex) {
-    return;
-  }
-  else if (index > slideIndex) {
-    direction = "right";
-  }
-  else {
-    direction = "left";
-  }
-  prevShown = slideIndex;
-  slideIndex = index;
-  ShowSlide(slideIndex, prevShown, direction);
-}
+});
